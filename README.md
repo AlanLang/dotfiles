7c9dae3ca4884a78625131d4fce226edf170e409
alan <langwdalan@gmail.com>
2023-09-10 11:33:32 +0800

    Add fish

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ git clone https://github.com/AlanLang/dotfile.git ~/dotfiles
 cd ~/dotfiles
 stow git
 ```
+
+## fish
+install fish
+```bash
+brew install fish
+```
+install fisher
+```bash
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+```
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,20 @@
+set fish_greeting ""
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme default
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
