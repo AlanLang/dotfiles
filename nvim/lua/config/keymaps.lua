1c137b9c9bd6b2e40befd51440bb744baf39e552
alan <langwdalan@gmail.com>
2026-03-24 16:02:49 +0800

    fix(nvim): 允许快速文件路径和行号

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 复制当前文件路径 + 行号（相对路径）
+vim.keymap.set("n", "<leader>yl", function()
+  local path = vim.fn.expand("%:.")
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+  local result = path .. ":" .. line
+  vim.fn.setreg("+", result)
+  vim.notify("已复制：" .. result)
+end, { desc = "Copy: 文件路径 + 行号" })
