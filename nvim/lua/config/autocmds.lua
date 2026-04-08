db759ec8c092608eb7e5e466782c467204061749
alan <langwdalan@gmail.com>
2026-04-08 19:34:25 +0800

    feat(nvim): 增加自动切换输入法的功能

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,26 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- 自动切换输入法：离开插入模式时切换到英文，进入插入模式时恢复之前的输入法
+local ime_autogroup = vim.api.nvim_create_augroup("ImeAutoGroup", { clear = true })
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = ime_autogroup,
+  callback = function()
+    vim.system({ "macism" }, { text = true }, function(out)
+      PREVIOUS_IM_CODE_MAC = string.gsub(out.stdout, "\n", "")
+    end)
+    vim.cmd(":silent :!macism com.apple.keylayout.ABC")
+  end,
+})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  group = ime_autogroup,
+  callback = function()
+    if PREVIOUS_IM_CODE_MAC then
+      vim.cmd(":silent :!macism " .. PREVIOUS_IM_CODE_MAC)
+    end
+    PREVIOUS_IM_CODE_MAC = nil
+  end,
+})
